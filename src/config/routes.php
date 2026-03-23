3813ad5d10c067f3276d387b0ffa6fcb56cc4f86
GoshaDev01 <acaatro21@gmail.com>
2026-03-23 11:03:54 +0300

    Delete src directory

--- src/config/routes.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-    '~^hello/(.*)$~' => [src\controllers\MainController::class, 'sayHello'],
-    '~^test/$~' => [src\controllers\TestController::class, 'view'],
-    '~^$~' => [src\controllers\MainController::class, 'main'],
-];
